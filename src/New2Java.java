0443ae0a7b2503b1116f0512f2b8fa30c8336a0b
Landon <N/A>
2024-09-04 21:01:07 -0500

    h

--- src/New2Java.java
+++ src/New2Java.java
@@ -1,7 +1,7 @@
 public class New2Java {
     public static void main(String[] args) {
 
-        System.out.println("I have the secret formula!");
+        System.out.println("I have the secret formula!!!!");
 
     }
 }
